e99cc1b418c6b3e9c612b7aa4b6e1f59f19e84eb
Thomas Krahn <ntbc@gmx.net>
2020-01-22 09:13:28 +0100

    Add boilerplate code for User tests

---
+++ user_test.go
@@ -0,0 +1,23 @@
+package client
+
+import "testing"
+
+func TestUserCreate(t *testing.T) {
+
+}
+
+func TestUserUpdate(t *testing.T) {
+
+}
+
+func TestUserChangePassword(t *testing.T) {
+
+}
+
+func TestUserRead(t *testing.T) {
+
+}
+
+func TestUserDelete(t *testing.T) {
+
+}
