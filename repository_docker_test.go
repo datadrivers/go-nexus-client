5de19e8292e8bd50c8e350c64832e2e654dfa094
Thomas Krahn <ntbc@gmx.net>
2020-01-30 12:58:30 +0100

    Make HTTPPort and HTTPSPort of Docker optional

--- repository_docker_test.go
+++ repository_docker_test.go
@@ -6,7 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func getTestRepositoryDocker(name string) Repository {
+func getTestRepositoryDockerWithPorts(name string) Repository {
+	httpPort := new(int)
+	httpsPort := new(int)
+	*httpPort = 8082
+	*httpsPort = 8083
+
 	return Repository{
 		Name:   name,
 		Online: true,
@@ -16,8 +21,8 @@ func getTestRepositoryDocker(name string) Repository {
 		RepositoryDocker: &RepositoryDocker{
 			V1Enabled:      false,
 			ForceBasicAuth: true,
-			HTTPPort:       8082,
-			HTTPSPort:      8083,
+			HTTPPort:       httpPort,
+			HTTPSPort:      httpsPort,
 		},
 		RepositoryStorage: &RepositoryStorage{
 			BlobStoreName:               "default",
@@ -27,9 +32,45 @@ func getTestRepositoryDocker(name string) Repository {
 	}
 }
 
-func TestRepositoryDockerHosted(t *testing.T) {
+func TestRepositoryDockerHostedWithPorts(t *testing.T) {
 	client := NewClient(getDefaultConfig())
-	repo := getTestRepositoryDocker("test-docker-repo-hosted")
+	repo := getTestRepositoryDockerWithPorts("test-docker-repo-hosted-with-ports")
+
+	err := client.RepositoryDockerCreate(repo, "hosted")
+	assert.Nil(t, err)
+
+	updatedRepo := repo
+	updatedRepo.Online = false
+
+	err = client.RepositoryDockerUpdate(repo.Name, updatedRepo, "hosted")
+	assert.Nil(t, err)
+
+	err = client.RepositoryDockerDelete(repo.Name)
+	assert.Nil(t, err)
+}
+
+func getTestRepositoryDockerWithoutPorts(name string) Repository {
+	return Repository{
+		Name:   name,
+		Online: true,
+		RepositoryCleanup: &RepositoryCleanup{
+			PolicyNames: []string{"weekly-cleanup"},
+		},
+		RepositoryDocker: &RepositoryDocker{
+			V1Enabled:      false,
+			ForceBasicAuth: true,
+		},
+		RepositoryStorage: &RepositoryStorage{
+			BlobStoreName:               "default",
+			StrictContentTypeValidation: true,
+			WritePolicy:                 "ALLOW_ONCE",
+		},
+	}
+}
+
+func TestRepositoryDockerHostedWithoutPorts(t *testing.T) {
+	client := NewClient(getDefaultConfig())
+	repo := getTestRepositoryDockerWithoutPorts("test-docker-repo-hosted-with-ports")
 
 	err := client.RepositoryDockerCreate(repo, "hosted")
 	assert.Nil(t, err)
