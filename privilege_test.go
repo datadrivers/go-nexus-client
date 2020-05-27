6d9ad0eb9489a3ac7270d25182bf6a97b5e31fd8
Thomas Krahn <ntbc@gmx.net>
2020-05-27 09:36:39 +0200

    Extend privilege test

--- privilege_test.go
+++ privilege_test.go
@@ -10,38 +10,43 @@ import (
 
 func TestPrivilegeCreateReadUpdateDelete(t *testing.T) {
 	client := NewClient(getDefaultConfig())
-	name := "test-privilege"
-	createdPrivilege := testPrivilege(name)
+	privilege := testPrivilege("test-privilege")
 
-	err := client.PrivilegeCreate(*createdPrivilege)
+	err := client.PrivilegeCreate(privilege)
 	assert.Nil(t, err)
 
-	readPrivilege, err := client.PrivilegeRead(name)
-	assert.Equal(t, createdPrivilege, readPrivilege)
-	assert.Nil(t, err)
+	if err != nil {
+		createdPrivilege, err := client.PrivilegeRead(privilege.Name)
+		assert.Nil(t, err)
+		assert.Equal(t, privilege.Name, createdPrivilege.Name)
+		assert.Equal(t, privilege.Description, createdPrivilege.Description)
+		assert.Equal(t, privilege.Domain, createdPrivilege.Domain)
+		assert.Equal(t, privilege.Type, createdPrivilege.Type)
 
-	updatedPrivilege := testPrivilege(name)
+		// Update
+		createdPrivilege.Description = "updated"
+		createdPrivilege.Domain = "datastores"
 
-	err = client.PrivilegeUpdate(name, *updatedPrivilege)
-	assert.Nil(t, err)
+		err = client.PrivilegeUpdate(privilege.Name, *createdPrivilege)
+		assert.Nil(t, err)
+		updatedPrivilege, err := client.PrivilegeRead(privilege.Name)
+		assert.Nil(t, err)
+		assert.Equal(t, createdPrivilege.Description, updatedPrivilege.Description)
+		assert.Equal(t, createdPrivilege.Domain, updatedPrivilege.Domain)
 
-	readPrivilege, err = client.PrivilegeRead(name)
-	assert.Equal(t, updatedPrivilege, readPrivilege)
-	assert.Nil(t, err)
+		err = client.PrivilegeDelete(privilege.Name)
+		assert.Nil(t, err)
 
-	err = client.PrivilegeDelete(name)
-	assert.Nil(t, err)
-
-	readPrivilege, err = client.PrivilegeRead(name)
-	assert.Nil(t, readPrivilege)
-	assert.Nil(t, err)
+		deletedPrivilege, err := client.PrivilegeRead(privilege.Name)
+		assert.Nil(t, err)
+		assert.Nil(t, deletedPrivilege)
+	}
 }
 
-func testPrivilege(name string) *Privilege {
-	time := time.Now().Unix()
-	return &Privilege{
+func testPrivilege(name string) Privilege {
+	return Privilege{
 		Actions:     []string{"READ"},
-		Description: fmt.Sprintf("Go client privilege %d", time),
+		Description: fmt.Sprintf("Go client privilege %d", time.Now().Unix()),
 		Domain:      "*",
 		Name:        name,
 		Type:        "application",
