f5575461674624a5d2f37a39bb68511687ba68bf
Thomas Krahn <ntbc@gmx.net>
2020-07-04 10:55:34 +0200

    Support security realms

---
+++ realm_test.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRealmsActivate(t *testing.T) {
+	client := NewClient(getDefaultConfig())
+	activeRealms, err := client.RealmsActive()
+	assert.Nil(t, err)
+	assert.NotNil(t, activeRealms)
+
+	err = client.RealmsActivate(activeRealms)
+	assert.Nil(t, err)
+}
+
+func TestRealmsActive(t *testing.T) {
+	client := NewClient(getDefaultConfig())
+	activeRealms, err := client.RealmsActive()
+	assert.Nil(t, err)
+	assert.NotNil(t, activeRealms)
+	assert.Equal(t, 2, len(activeRealms))
+	assert.Equal(t, "NexusAuthenticatingRealm", activeRealms[0])
+	assert.Equal(t, "NexusAuthorizingRealm", activeRealms[1])
+}
+
+func TestRealmsAvailable(t *testing.T) {
+	client := NewClient(getDefaultConfig())
+	availableRealms, err := client.RealmsAvailable()
+	assert.Nil(t, err)
+	assert.NotNil(t, availableRealms)
+	assert.Greater(t, len(availableRealms), 0)
+
+	for _, realm := range availableRealms {
+		assert.NotNil(t, realm.ID)
+		assert.NotNil(t, realm.Name)
+	}
+}
