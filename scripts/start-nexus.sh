c06d6b77978720d9f9c4d3c0d7e2ba805e3d0eee
Thomas Krahn <ntbc@gmx.net>
2020-08-03 16:55:03 +0200

    Use Nexus 3.25.1 for testing and bring back default admin password as nexus.security.randompassword=false seems not to work

--- scripts/start-nexus.sh
+++ scripts/start-nexus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 LOCAL_PORT=8081
-NEXUS_VERSION=3.24.0
+NEXUS_VERSION=3.25.1
 
 echo "Starting Nexus container..."
-docker run -d --rm -v "${PWD}"/nexus.properties:/nexus-data/etc/nexus.properties \
+docker run -d --rm -v "${PWD}/nexus.properties":/nexus-data/etc/nexus.properties \
   --name nexus -p 127.0.0.1:${LOCAL_PORT}:8081 sonatype/nexus3:${NEXUS_VERSION}
 
 echo -n "Waiting for Nexus to be ready "
@@ -16,3 +16,9 @@ do
     if [[ $((i%3)) == 0 ]]; then echo -n ' '; fi
     (( i++ ))
 done
+
+echo "Getting admin password..."
+NEXUS_ADMIN_PASSWORD=$(docker exec -ti nexus cat /nexus-data/admin.password)
+
+echo "Setting admin password..."
+curl -X PUT "http://127.0.0.1:${LOCAL_PORT}/service/rest/beta/security/users/admin/change-password" -H "accept: application/json" -H "Content-Type: text/plain" -d "admin123" -u "admin:${NEXUS_ADMIN_PASSWORD}"
