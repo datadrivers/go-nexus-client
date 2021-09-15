0c072f8ad514c913c0e0541d80bb88c46d6427aa
shentuzhigang <shentuzhigang@moresec.cn>
2021-09-15 14:48:12 +0800

    dev: add some test for asset and component

--- asset_test.go
+++ asset_test.go
@@ -1,1 +1,21 @@
 package client
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAssetRead(t *testing.T) {
+	client := getTestClient()
+
+	id := ""
+
+	asset, err := client.AssetRead(id)
+	assert.Nil(t, err)
+	assert.NotNil(t, asset)
+
+	if asset != nil {
+		assert.Equal(t, id, asset.ID)
+		assert.Equal(t, "maven-central", asset.Repository)
+	}
+}
--- component_test.go
+++ component_test.go
@@ -1,1 +1,21 @@
 package client
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestComponentReadRead(t *testing.T) {
+	client := getTestClient()
+
+	id := ""
+
+	component, err := client.ComponentRead(id)
+	assert.Nil(t, err)
+	assert.NotNil(t, component)
+
+	if component != nil {
+		assert.Equal(t, id, component.ID)
+		assert.Equal(t, "maven-central", component.Repository)
+	}
+}
