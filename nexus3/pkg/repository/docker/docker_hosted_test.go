41f9ee23bac4d1b104e08aa983c68038b3640549
Robert Tribit <Robert.Tribit@citizensbank.com>
2024-01-29 13:03:42 -0600

    Adding in pro skippable tests.

--- nexus3/pkg/repository/docker/docker_hosted_test.go
+++ nexus3/pkg/repository/docker/docker_hosted_test.go
@@ -32,6 +32,32 @@ func getTestDockerHostedRepository(name string) repository.DockerHostedRepositor
 			V1Enabled:      false,
 			HTTPPort:       tools.GetIntPointer(8180),
 			HTTPSPort:      tools.GetIntPointer(8543),
+		},
+	}
+}
+
+func getTestProDockerHostedRepository(name string) repository.DockerHostedRepository {
+	writePolicy := repository.StorageWritePolicyAllow
+	return repository.DockerHostedRepository{
+		Name:   name,
+		Online: true,
+
+		Cleanup: &repository.Cleanup{
+			PolicyNames: []string{"weekly-cleanup"},
+		},
+		Storage: repository.HostedStorage{
+			BlobStoreName:               "default",
+			StrictContentTypeValidation: true,
+			WritePolicy:                 &writePolicy,
+		},
+		Component: &repository.Component{
+			ProprietaryComponents: true,
+		},
+		Docker: repository.Docker{
+			ForceBasicAuth: true,
+			V1Enabled:      false,
+			HTTPPort:       tools.GetIntPointer(8180),
+			HTTPSPort:      tools.GetIntPointer(8543),
 			Subdomain:      tools.GetStringPointer(name),
 		},
 	}
@@ -40,6 +66,38 @@ func getTestDockerHostedRepository(name string) repository.DockerHostedRepositor
 func TestDockerHostedRepository(t *testing.T) {
 	service := getTestService()
 	repo := getTestDockerHostedRepository("test-docker-repo-hosted-" + strconv.Itoa(rand.Intn(1024)))
+
+	err := service.Hosted.Create(repo)
+	assert.Nil(t, err)
+	generatedRepo, err := service.Hosted.Get(repo.Name)
+	assert.Nil(t, err)
+	assert.Equal(t, repo.Online, generatedRepo.Online)
+	assert.Equal(t, repo.Cleanup, generatedRepo.Cleanup)
+	assert.Equal(t, repo.Storage, generatedRepo.Storage)
+	assert.Equal(t, repo.Component, generatedRepo.Component)
+	assert.Equal(t, repo.Docker, generatedRepo.Docker)
+
+	updatedRepo := repo
+	updatedRepo.Online = false
+	updatedRepo.V1Enabled = true
+
+	err = service.Hosted.Update(repo.Name, updatedRepo)
+	assert.Nil(t, err)
+	generatedRepo, err = service.Hosted.Get(updatedRepo.Name)
+	assert.Nil(t, err)
+	assert.Equal(t, updatedRepo.Online, generatedRepo.Online)
+	assert.Equal(t, updatedRepo.V1Enabled, generatedRepo.V1Enabled)
+
+	service.Hosted.Delete(repo.Name)
+	assert.Nil(t, err)
+}
+
+func TestProDockerHostedRepository(t *testing.T) {
+	if tools.GetEnv("SKIP_PRO_TESTS", "false") == "true" {
+		t.Skip("Skipping Nexus Pro tests")
+	}
+	service := getTestService()
+	repo := getTestProDockerHostedRepository("test-docker-repo-hosted-" + strconv.Itoa(rand.Intn(1024)))
 
 	err := service.Hosted.Create(repo)
 	assert.Nil(t, err)
