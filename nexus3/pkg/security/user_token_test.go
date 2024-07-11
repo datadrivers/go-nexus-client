04a431f46961169e7e25e1b15301364dbeb5f04c
Christian Sibbel <christian.sibbel@datadrivers.de>
2024-07-11 14:47:04 +0200

    feat: Add user token expiration

--- nexus3/pkg/security/user_token_test.go
+++ nexus3/pkg/security/user_token_test.go
@@ -15,28 +15,32 @@ func TestUserTokens(t *testing.T) {
 	service := getTestService()
 
 	userTokens := security.UserTokenConfiguration{
-		Enabled:        true,
-		ProtectContent: true,
-		ExpirationDays: int(45),
+		Enabled:           true,
+		ProtectContent:    true,
+		ExpirationEnabled: true,
+		ExpirationDays:    int(45),
 	}
 	err := service.UserTokens.Configure(userTokens)
 	assert.Nil(t, err)
 	createdUserTokens, err := service.UserTokens.Get()
 	assert.Nil(t, err)
 	assert.NotNil(t, createdUserTokens)
-	assert.Equal(t, userTokens.ExpirationDays, createdUserTokens.ExpirationDays)
 	assert.Equal(t, userTokens.Enabled, createdUserTokens.Enabled)
 	assert.Equal(t, userTokens.ProtectContent, createdUserTokens.ProtectContent)
+	assert.Equal(t, userTokens.ExpirationEnabled, createdUserTokens.ExpirationEnabled)
+	assert.Equal(t, userTokens.ExpirationDays, createdUserTokens.ExpirationDays)
 
-	createdUserTokens.ExpirationDays = int(30)
 	createdUserTokens.ProtectContent = false
+	createdUserTokens.ExpirationEnabled = false
+	createdUserTokens.ExpirationDays = int(30)
 	err = service.UserTokens.Configure(*createdUserTokens)
 	assert.Nil(t, err)
 
 	updatedUserTokens, err := service.UserTokens.Get()
 	assert.Nil(t, err)
 	assert.NotNil(t, updatedUserTokens)
-	assert.Equal(t, createdUserTokens.ExpirationDays, updatedUserTokens.ExpirationDays)
 	assert.Equal(t, createdUserTokens.ProtectContent, updatedUserTokens.ProtectContent)
+	assert.Equal(t, createdUserTokens.ExpirationEnabled, updatedUserTokens.ExpirationEnabled)
+	assert.Equal(t, createdUserTokens.ExpirationDays, updatedUserTokens.ExpirationDays)
 
 }
--- nexus3/schema/security/user-token.go
+++ nexus3/schema/security/user-token.go
@@ -2,7 +2,8 @@ package security
 
 // UserTokenConfiguration data structure
 type UserTokenConfiguration struct {
-	Enabled        bool `json:"enabled"`
-	ProtectContent bool `json:"protectContent"`
-	ExpirationDays int  `json:"expirationDays"`
+	Enabled           bool `json:"enabled"`
+	ProtectContent    bool `json:"protectContent"`
+	ExpirationEnabled bool `json:"expirationEnabled"`
+	ExpirationDays    int  `json:"expirationDays"`
 }
