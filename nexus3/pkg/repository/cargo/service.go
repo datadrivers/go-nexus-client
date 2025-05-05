79b77fafe0db927737eda5d91c1d362354a13049
Đặng Minh Dũng <dungdm93@live.com>
2025-05-05 17:21:44 +0700

    chores: remove duplicate code in `repository` package by using generics

--- nexus3/pkg/repository/cargo/service.go
+++ nexus3/pkg/repository/cargo/service.go
@@ -3,10 +3,15 @@ package cargo
 import (
 	"github.com/datadrivers/go-nexus-client/nexus3/pkg/client"
 	"github.com/datadrivers/go-nexus-client/nexus3/pkg/repository/common"
+	"github.com/datadrivers/go-nexus-client/nexus3/schema/repository"
 )
 
-const (
-	cargoAPIEndpoint = common.RepositoryAPIEndpoint + "/cargo"
+const cargoAPIEndpoint = common.RepositoryAPIEndpoint + "/cargo"
+
+type (
+	RepositoryCargoGroupService  = common.RepositoryService[repository.CargoGroupRepository]
+	RepositoryCargoHostedService = common.RepositoryService[repository.CargoHostedRepository]
+	RepositoryCargoProxyService  = common.RepositoryService[repository.CargoProxyRepository]
 )
 
 type RepositoryCargoService struct {
@@ -25,4 +30,16 @@ func NewRepositoryCargoService(c *client.Client) *RepositoryCargoService {
 		Proxy:  NewRepositoryCargoProxyService(c),
 		Hosted: NewRepositoryCargoHostedService(c),
 	}
+}
+
+func NewRepositoryCargoGroupService(c *client.Client) *RepositoryCargoGroupService {
+	return common.NewRepositoryService[repository.CargoGroupRepository](cargoAPIEndpoint+"/group", c)
+}
+
+func NewRepositoryCargoHostedService(c *client.Client) *RepositoryCargoHostedService {
+	return common.NewRepositoryService[repository.CargoHostedRepository](cargoAPIEndpoint+"/hosted", c)
+}
+
+func NewRepositoryCargoProxyService(c *client.Client) *RepositoryCargoProxyService {
+	return common.NewRepositoryService[repository.CargoProxyRepository](cargoAPIEndpoint+"/proxy", c)
 }
